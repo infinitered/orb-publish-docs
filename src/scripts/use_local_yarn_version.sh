eff6af188b084aa3655049ec668c18fd4126150f
Trevor Coleman <trevor@trevorcoleman.design>
2023-09-28 13:56:29 -0400

    deglob

--- src/scripts/use_local_yarn_version.sh
+++ src/scripts/use_local_yarn_version.sh
@@ -1,4 +1,5 @@
 #! /bin/bash
 
 # Set the PATH using the environment variable
-echo "export PATH=$INSTALL_YARN_DEPS_WORKING_DIR/.yarn/releases:$PATH" >> $BASH_ENV
+echo "export PATH=$INSTALL_YARN_DEPS_WORKING_DIR/.yarn/releases:$PATH" >> "$BASH_ENV"
+

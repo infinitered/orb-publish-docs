067c03652a76da1699f13cf7026d22d78d19c91c
Trevor Coleman <trevor@trevorcoleman.design>
2023-10-04 14:07:06 -0400

    store artifacts, fix copying and other fixes (#13)
    * store artifcts
    
    * fix copying and links

--- src/scripts/copy_docs.sh
+++ src/scripts/copy_docs.sh
@@ -2,7 +2,15 @@
 
 CopyDocs() {
   echo "Copying documents to the target repository."
-  cp -R "$FULL_SOURCE_DOCS_PATH" "$TARGET_REPO_DIRECTORY/$TARGET_DOCS_PATH/$PROJECT_NAME"
+  echo "Source: $FULL_SOURCE_DOCS_PATH"
+  echo "Destination: $FULL_TARGET_DOCS_PATH"
+
+  echo "Files to be copied:"
+  # Log the list of files to be copied
+  find "$FULL_SOURCE_DOCS_PATH" -type f -print
+  echo "----"
+  echo "Copying files..."
+  cp -R "$FULL_SOURCE_DOCS_PATH" "$FULL_TARGET_DOCS_PATH/$PROJECT_NAME"
   echo "Documents copied successfully."
 }
 
--- src/scripts/create_category_json.sh
+++ src/scripts/create_category_json.sh
@@ -2,20 +2,20 @@
 
 # Assumes LABEL, DESCRIPTION, TARGET_REPO_DIRECTORY, and PROJECT_NAME are environment variables
 
-CreateProjectJSONFromTemplate() {
+CreateCategoryJSON() {
     # Ensure the target directory exists
     mkdir -p "$TARGET_REPO_DIRECTORY/docs/$PROJECT_NAME/"
 
     # Inlined template with placeholders
     # shellcheck disable=SC2016
-    TEMPLATE='{"label": "${LABEL}","description": "${DESCRIPTION}", "projectName":"${PROJECT_NAME}"}'
+    TEMPLATE='{"label": "${LABEL}","link": {"type": "generated-index","title": "${LABEL}"},"customProps": {"description": "${DESCRIPTION}", "projectName":"${PROJECT_NAME}"}}'
 
 # Substitute the variables in the template and write to target location
     echo "$TEMPLATE" | sed \
         -e "s/\${LABEL}/$LABEL/" \
         -e "s/\${DESCRIPTION}/$DESCRIPTION/" \
         -e "s/\${PROJECT_NAME}/$PROJECT_NAME/" \
-        > "$TARGET_REPO_DIRECTORY/docs/$PROJECT_NAME/_project_.json"
+        > "$TARGET_REPO_DIRECTORY/docs/$PROJECT_NAME/_category_.json"
 
     echo "_project_.json file created successfully."
 }
@@ -23,5 +23,10 @@ CreateProjectJSONFromTemplate() {
 # Check for bats
 ORB_TEST_ENV="bats-core"
 if [ "${0#*"$ORB_TEST_ENV"}" = "$0" ]; then
-    CreateProjectJSONFromTemplate
+    CreateCategoryJSON
 fi
+
+
+
+
+

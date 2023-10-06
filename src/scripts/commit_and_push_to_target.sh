ce45ef4df472e336a4d9c6df8e8c38a503811b92
Trevor Coleman <trevor@trevorcoleman.design>
2023-10-06 15:45:46 -0400

    fix commit step

--- src/scripts/commit_and_push_to_target.sh
+++ src/scripts/commit_and_push_to_target.sh
@@ -2,10 +2,18 @@
 
 CommitAndPushToTarget () {
 cd "$TARGET_REPO_DIRECTORY" || { echo "Changing directory failed"; exit 1; }
+
 git add docs || { echo "Git add docs failed"; exit 1; }
 git add static || { echo "Git add static failed"; exit 1; }
-git commit -m "\"$FINAL_COMMIT_MESSAGE\"" || { echo "Git commit failed"; exit 1; }
-git push origin main || { echo "Git push failed"; exit 1; }
+
+if git diff-index --quiet HEAD --; then
+    echo "No changes"
+    exit 0
+
+else
+    git commit -m "\"$FINAL_COMMIT_MESSAGE\"" || { echo "Git commit failed"; exit 1; }
+    git push origin main || { echo "Git push failed"; exit 1; }
+fi
 }
 
 ORB_TEST_ENV="bats-core"

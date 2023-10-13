eddd5dbf69ab2e1b35b18a1eb894b3f375d7bc7e
Trevor Coleman <trevor@trevorcoleman.design>
2023-10-13 15:47:54 -0400

    add exit

--- src/scripts/fetch_commit_info.sh
+++ src/scripts/fetch_commit_info.sh
@@ -1,26 +1,27 @@
 #!/bin/bash
+set -x  # Enable debugging output
 
-# Function to change to the source repository directory
 ChangeToSourceRepoDirectory() {
-  cd "$SOURCE_REPO_DIRECTORY" || { echo "Changing directory failed" >&2; exit 1; }
+  echo "Changing directory to $SOURCE_REPO_DIRECTORY" >&2
+  cd "$SOURCE_REPO_DIRECTORY" || { echo "Changing directory to $SOURCE_REPO_DIRECTORY failed" >&2; exit 1; }
 }
 
-
-# Function to fetch commit information and extract PR number
 FetchCommitInfo() {
   local PR_NUMBER
   local COMMIT_MESSAGE
   local COMMIT_HASH
 
-  # Fetch the last commit message and hash
   COMMIT_MESSAGE=$(git log -1 --pretty=%B || { echo "Fetching commit message failed" >&2 ; exit 1; })
   COMMIT_HASH=$(git rev-parse HEAD || { echo "Fetching commit hash failed" >&2 ; exit 1; })
 
-  # Extract PR number from the commit message using regex
   PR_NUMBER=$(echo "$COMMIT_MESSAGE" | grep -oP '(Merge pull request #\K\d+)|(\(#\K\d+\))')
-  PR_NUMBER=${PR_NUMBER:-""} # Set to empty string if no match
+  PR_NUMBER=${PR_NUMBER:-""}
 
-  # Export the variables
+  if [ -z "$BASH_ENV" ]; then
+    echo "BASH_ENV is not set" >&2
+    exit 1
+  fi
+
   {
     echo "export COMMIT_MESSAGE=\"${COMMIT_MESSAGE}\""
     echo "export COMMIT_HASH=\"${COMMIT_HASH}\""
@@ -28,9 +29,10 @@ FetchCommitInfo() {
   } >> "$BASH_ENV"
 }
 
-# Only call the function if not in a Bats test environment
 ORB_TEST_ENV="bats-core"
 if [ "${0#*"$ORB_TEST_ENV"}" = "$0" ]; then
   ChangeToSourceRepoDirectory
   FetchCommitInfo
 fi
+
+exit 0

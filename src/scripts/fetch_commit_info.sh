ef0d7dba4fbdb1ebd6260bb28a61ae3bc302e165
Trevor Coleman <trevor@trevorcoleman.design>
2023-10-13 16:02:19 -0400

    remove bash env check

--- src/scripts/fetch_commit_info.sh
+++ src/scripts/fetch_commit_info.sh
@@ -12,27 +12,30 @@ FetchCommitInfo() {
   local COMMIT_HASH
 
   COMMIT_MESSAGE=$(git log -1 --pretty=%B || { echo "Fetching commit message failed" >&2 ; exit 1; })
+  echo "COMMIT_MESSAGE: $COMMIT_MESSAGE" >&2
   COMMIT_HASH=$(git rev-parse HEAD || { echo "Fetching commit hash failed" >&2 ; exit 1; })
+  echo "COMMIT_HASH: $COMMIT_HASH" >&2
 
   PR_NUMBER=$(echo "$COMMIT_MESSAGE" | grep -oP '(Merge pull request #\K\d+)|(\(#\K\d+\))')
   PR_NUMBER=${PR_NUMBER:-""}
 
-  if [ -z "$BASH_ENV" ]; then
-    echo "BASH_ENV is not set" >&2
-    exit 1
-  fi
+  # shellcheck disable=SC2086
 
-  {
-    echo "export COMMIT_MESSAGE=\"${COMMIT_MESSAGE}\""
-    echo "export COMMIT_HASH=\"${COMMIT_HASH}\""
-    echo "export PR_NUMBER=\"$PR_NUMBER\""
-  } >> "$BASH_ENV"
+  echo "COMMIT_MESSAGE: $COMMIT_MESSAGE" >&2
+  echo "export COMMIT_MESSAGE=\"${COMMIT_MESSAGE}\"" >> $BASH_ENV
+  echo "COMMIT_HASH: $COMMIT_HASH" >&2
+  echo "export COMMIT_HASH=\"${COMMIT_HASH}\"" >> $BASH_ENV
+  echo "PR_NUMBER: $PR_NUMBER" >&2
+  echo "export PR_NUMBER=\"$PR_NUMBER\"" >> $BASH_ENV
+
+  exit 0
 }
 
 ORB_TEST_ENV="bats-core"
 if [ "${0#*"$ORB_TEST_ENV"}" = "$0" ]; then
   ChangeToSourceRepoDirectory
   FetchCommitInfo
+  exit  0
 fi
 
 exit 0
